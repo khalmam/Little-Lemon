a88bf5c98e251e2a2d5166835a7e5ae827af454f
khalboy <genkhal25@gmail.com>
2024-07-10 19:47:19 +0100

    .

--- LittleLemon/restaurant/urls.py
+++ LittleLemon/restaurant/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin 
 from django.urls import path 
-from . import views,sayHello 
+from . import views
   
 urlpatterns = [ 
-    path('', sayHello, name='sayHello'), 
     path('', views.index, name='index'),
 ]
