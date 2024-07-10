44fc50596c5dd9831f85c6be9008a68c9cd6d536
khalboy <genkhal25@gmail.com>
2024-07-10 20:28:49 +0100

    created models for booking and menu

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Booking(models.Model):
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
     bokingdate = models.DateField()
@@ -12,7 +12,7 @@ class Booking(models.Model):
     
     
 class Menu(models.Model):
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2,)
     inventory = models.IntegerField()
