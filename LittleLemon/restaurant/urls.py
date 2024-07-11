3b2c41215337619cc0db7e7fd9fe46d46c599708
khalboy <genkhal25@gmail.com>
2024-07-11 13:59:34 +0100

    updated urls.py of

--- LittleLemon/restaurant/urls.py
+++ LittleLemon/restaurant/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin 
-from django.urls import path 
+from django.urls import path , include
 from . import views
 from rest_framework.routers import DefaultRouter
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
 ]
