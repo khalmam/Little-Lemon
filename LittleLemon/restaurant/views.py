d162f02fc30c321f2a5aa5410157d3c4a6cca30d
khalboy <genkhal25@gmail.com>
2024-07-11 08:32:36 +0100

    updated views

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rest_framework.decorators import api_view
+from .models import MenuItem
+from.serializers import MenuItemSerializer
 
 
 # Create your views here.
@@ -8,3 +10,13 @@ def index(request):
   return render(request, 'index.html', {})
 def sayHello(request):
  return HttpResponse('Hello World')
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+     queryset = MenuItem.objects.all() 
+     serializer_class = MenuItemSerializer
