5138915f44056621dbf9524b09b78af78866062a
khalboy <genkhal25@gmail.com>
2024-07-12 16:32:52 +0100

    added authtoken

--- LittleLemon/LittleLemon/urls.py
+++ LittleLemon/LittleLemon/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin 
 from django.urls import path, include  
-from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token #import obtain_auth_token view
+
 
 urlpatterns = [ 
     path('admin/', admin.site.urls), 
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include('restaurant.urls')),
+      path('api-token-auth/', obtain_auth_token),
 ]
