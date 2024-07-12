e422ac0583e2ce2a9921d895f0bbdc1d00cda5dd
khalboy <genkhal25@gmail.com>
2024-07-12 15:47:08 +0100

    added new model MenuItem

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -18,3 +18,10 @@ class Menu(models.Model):
     inventory = models.IntegerField()
 
     
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'  
